e02402d6fc549cd82d5a22fa705bb4a692fa88df
vanyognqi <vanyongqi@gmail.com>
2022-03-30 21:00:53 -0700

    client code

--- src/client/command.hpp
+++ src/client/command.hpp
@@ -1,5 +1,4 @@
 /*******************************************************************************
-   Copyright (C) 2013 SequoiaDB Software Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License, version 3,
