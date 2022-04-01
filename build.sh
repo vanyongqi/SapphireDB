23c786b25576654ea5f8dd59b5d65047613d06d9
vanyognqi <vanyongqi@gmail.com>
2022-03-31 19:45:03 -0700

    basic development tools

---
+++ build.sh
@@ -0,0 +1,11 @@
+sudo apt-get install git
+sudo apt-get install g++
+sudo apt-get install gdb
+sudo apt-get install vim
+sudo apt-get install autoconf
+sudo apt-get install libtool
+sudo apt-get install make
+sudo apt-get install unzip
+sudo apt-get install x11-apps
+sudo apt-get install build-essential g++ python-dev lib-icu-dev buiild essential libbz2-dev libboost-all-dev
+
