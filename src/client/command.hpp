c23b2162b376216f4ca103909395c7fc6e5fd09c
vanyognqi <vanyongqi@gmail.com>
2022-04-20 01:27:15 -0700

    command interface and other command like insert delete and so on

--- src/client/command.hpp
+++ src/client/command.hpp
@@ -1,16 +1,9 @@
 /*******************************************************************************
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License, version 3,
-   as published by the Free Software Foundation.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU Affero General Public License for more details.
-
-   You should have received a copy of the GNU Affero General Public License
-   along with this program. If not, see <http://www.gnu.org/license/>.
+command.hpp
+ICommand是所有命令的接口，所有命令都要继承这个基类。
+execute负责和数据库引擎进程建立tcp连接，然后发送数据库操作命令。
+函数handleReply是用来处理数据库引擎服务器返回的数据的。
 *******************************************************************************/
 #ifndef _COMMAND_HPP_
 #define _COMMAND_HPP_
